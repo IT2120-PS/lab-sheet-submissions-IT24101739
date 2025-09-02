eefb09e102f51b91ced045d824bb37edfd02a2e6
IT24101739 <it24101739@my.sliit.lk>
2025-09-02 22:15:55 +0530

    Lab 06

---
+++ Lab Sheet 06/IT24101739 Lab 06.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\Maleesha\\OneDrive\\Desktop\\IT24101739\\Lab 06")
+getwd()
+
+#Question 01
+#1)Binomial Distribution
+#2)
+1-pbinom(46,50,0.85,lower.tail=TRUE)
+
+#Question 02
+#1)Number of calls per Hour
+#2)Poisson Distribution
+#3)
+dpois(15,12)
